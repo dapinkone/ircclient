556e9b1c50898fe4add9427093bb15b0a2b14354
DaPinkOne <crystalshade@gmail.com>
2017-09-03 18:00:14 -0400

    first major milestone, joins init channel.

--- ircclient.py
+++ ircclient.py
@@ -7,6 +7,7 @@ Planned milestones:
 3. reasonable formatting for output of data from socket
 4. SSL/SASL support
 5. addition of ncurse or similar gui, to graphically separate input and output
+   requires asyncio?
 6. support for /join, /part, /quit like most IRC clients have
 7. proper parsing of user input, rather than input->socket
 8. status line - showing which channels are currently joined, nick, etc
@@ -18,13 +19,36 @@ Planned milestones:
 12. think of more stuff to add.
 """
 import socket # there may be a more fully featured irc module. #research
-ircsock = socket.socket(socket.AF_I_Inet, socket.SOCK_STREAM)
+ircsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server = "chat.freenode.net"
-channel = "#nobodyhome"
+init_channel = "#nobodyhome"
 username = "simpleclient"
 port = 6667 #TODO add handling for cmdline args, handling of SASL port
 
+def encode_send(msg):
+    ircsock.send(bytes(msg, "UTF-8"))
+
 ircsock.connect((server, port))
 # TODO: write generalized function for ircsock.send("...\n")
-ircsock.send("USER {} {} {} {}\n".format(*[username]*4))
-ircsock.send("NICK {}\n".format(username))
+encode_send("USER {} {} {} {}\n".format(*[username]*4))
+encode_send("NICK {}\n".format(username))
+
+def join_channel(chan):
+    encode_send("JOIN {}\n".format(chan))
+
+join_channel(init_channel)
+def pong():
+    encode_send("PONG :pingis\n")
+    print("PONG")
+
+def privmsg(msg, target=init_channel):
+    encode_send("PRIVMSG {} :{}\n".format(target, msg))
+
+while True: #mainloop
+    incoming_msg = ircsock.recv(2048)
+    msglist = incoming_msg.split(b'\n')
+    for msg in msglist:
+        msg.strip()
+        if b'PING' in msg: # should be more specific
+            pong()
+        print(msg)
