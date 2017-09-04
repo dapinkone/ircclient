3f8a301aa957417bbeef8d093bfa9db5e65c1dce
DaPinkOne <crystalshade@gmail.com>
2017-09-04 03:40:10 -0400

    had input working, but it blocks. I dunno, but there has to be a solution. I will find it another time. removed for now.

--- ircclient.py
+++ ircclient.py
@@ -20,12 +20,16 @@ DONE: 1. connect to server, respond to ping so as not to timeout.
 """
 import asyncio # required for semi-simultaneous handling of io
 import sys
+
 async def wait_for_data(loop):
     server = "chat.freenode.net"
     init_channel = "#nobodyhome"
     username = "simplecli"
     port = 6667 # TODO add handling for cmdline args, handling of SASL port?
-    password = ''
+    authdata = open('./authdata.txt', 'r')
+    password = authdata.readline()
+    authdata.close()
+
     sockreader, sockwriter = await asyncio.open_connection(
         server, port, loop=loop)
     print("Socket connected.\n{}".format(repr(sockreader)))
@@ -33,15 +37,19 @@ async def wait_for_data(loop):
     def encode_send(msg):
         sockwriter.write(bytes(msg + "\r\n", "UTF-8"))
         print("> " + msg)
-
+    async def parse_input(msg):
+        print(msg)
+        if 'quit' in msg:
+            encode_send('QUIT')
+            quit()
     encode_send("USER {} {} {} {}".format(*[username]*4))
     encode_send("NICK {}".format(username))
     encode_send("JOIN {}".format(init_channel))
+
     while True:
         data = await sockreader.read(n=2048)
         msglist = str(data,"UTF-8").split('\r\n')
         for msg in msglist:
-            # msg.strip()
             print(msg)
             if 'PING' in msg[:4]:
                 encode_send("PONG{}".format(msg[4:]))
@@ -49,44 +57,13 @@ async def wait_for_data(loop):
                and ('This nickname is registered' in msg):
                 encode_send("privmsg nickserv :identify " +
                             username + " " + password)
-                #TODO store this data elsewhere. srsly.
-        await asyncio.sleep(0)
-        user_input = await take_input(loop)
-        if 'quit' in user_input:
-            encode_send('QUIT')
-            quit()
-    sockwriter.close()
 
-async def take_input(loop):
-    while True:
-        line = await loop.run_in_executor(None, sys.stdin.readline)
-        print('got line:', line, end='')
-        return line
+            if '!quit' in msg:
+                encode_send('quit')
+                quit()
+        await asyncio.sleep(0)
+    sockwriter.close()
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(wait_for_data(loop))
-loop.run_until_complete(take_input(loop))
 loop.close()
-
-# def join_channel(chan):
-#     encode_send("JOIN {}\n".format(chan))
-
-# join_channel(init_channel)
-# def pong(msg):
-#     encode_send("PONG{}\n".format(msg))
-#     print("PONG{}\n".format(msg))
-
-# def privmsg(msg, target=init_channel):
-#     encode_send("PRIVMSG {} :{}\n".format(target, msg))
-
-
-# while True: #mainloop
-#     incoming_msg = str(ircsock.recv(2048), "UTF-8")
-#     msglist = incoming_msg.split()
-#     for msg in msglist:
-#         msg.strip()
-#         if not msg: # if it's an empty line, move on.
-#             continue
-#         if 'PING' in msg[0:4]:
-#             pong(msg[4:])
-#         print(msg)
