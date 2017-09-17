73172a6428733327b2fb00784d0c0dd6bfb81c68
DaPinkOne <crystalshade@gmail.com>
2017-09-17 16:36:14 -0400

    trying to implement async input handling using prompt_toolkit. currently input is broken.

--- ircclient.py
+++ ircclient.py
@@ -3,7 +3,7 @@
 Irc client. lets take it slow.
 Planned milestones:
 DONE: 1. connect to server, respond to ping so as not to timeout.
-2. take input to be sent to the server
+2. take  input to be sent to the server
 3. reasonable formatting for output of data from socket
 4. SSL/SASL support
 5. addition of ncurse or similar gui, to graphically separate input and output
@@ -18,14 +18,39 @@ DONE: 1. connect to server, respond to ping so as not to timeout.
 11. /load script.py, and /scripts/ directory for autoloading python extensions
 12. think of more stuff to add.
 """
-import asyncio # required for semi-simultaneous handling of io
+import asyncio
+from prompt_toolkit.interface import CommandLineInterface
+from prompt_toolkit.shortcuts import create_prompt_application, create_asyncio_eventloop
+# pep8 pls ;_;
 import sys
+import logging
+
+loop = asyncio.get_event_loop()
+loop.set_debug(True)
+
+async def interactive_shell(): # the gui thread?
+    # jonothanslenders refers to `interactive_shell`? better solution?
+    # credit to him @ python-prompt-toolkit for this part
+    eventloop = create_asyncio_eventloop() # is this a second eventloop?
+
+    cli = CommandLineInterface(
+        application=create_prompt_application('Prompt: '),
+        eventloop=eventloop)
+
+    sys.stdout = cli.stdout_proxy() # is this a good idea?
+
+    while True:
+        try:
+            result = await cli.run_async() # it takes input! :D
+            print('You said: {}'.format(result.text))
+        except (EOFError, KeyboardInterrupt):
+            return
 
 async def wait_for_data(loop):
     server = "chat.freenode.net"
     init_channel = "#nobodyhome"
     username = "simplecli"
-    port = 6667 # TODO add handling for cmdline args, handling of SASL port?
+    port = 6667  # TODO add handling for cmdline args, handling of SASL port?
     authdata = open('./authdata.txt', 'r')
     password = authdata.readline()
     authdata.close()
@@ -37,18 +62,20 @@ async def wait_for_data(loop):
     def encode_send(msg):
         sockwriter.write(bytes(msg + "\r\n", "UTF-8"))
         print("> " + msg)
-    async def parse_input(msg):
+
+    def parse_input(msg):
         print(msg)
         if 'quit' in msg:
             encode_send('QUIT')
             quit()
-    encode_send("USER {} {} {} {}".format(*[username]*4))
+
+    encode_send("USER {} {} {} {}".format(*[username] * 4))
     encode_send("NICK {}".format(username))
     encode_send("JOIN {}".format(init_channel))
 
     while True:
         data = await sockreader.read(n=2048)
-        msglist = str(data,"UTF-8").split('\r\n')
+        msglist = str(data, "UTF-8").split('\r\n')
         for msg in msglist:
             print(msg)
             if 'PING' in msg[:4]:
@@ -61,9 +88,10 @@ async def wait_for_data(loop):
             if '!quit' in msg:
                 encode_send('quit')
                 quit()
-        await asyncio.sleep(0)
+        await asyncio.sleep(0.01)
     sockwriter.close()
 
-loop = asyncio.get_event_loop()
+loop.create_task(interactive_shell)
 loop.run_until_complete(wait_for_data(loop))
+
 loop.close()
