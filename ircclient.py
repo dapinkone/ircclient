6cadd319ecbf53f3363b49ef4a2e0b88f947a55c
DaPinkOne <crystalshade@gmail.com>
2017-09-22 10:56:17 -0400

    commented out gui/prompt-toolkit coroutines temporarily. In-process of converting code to be an agent class to prep for future multiple instances or multiple connections. code is currently functioning well as an IRC bot.

--- ircclient.py
+++ ircclient.py
@@ -36,11 +36,12 @@ password = authdata.readline()
 authdata.close()
 
 
-async def main(loop):
-    server = "chat.freenode.net"
-    init_channel = "#nobodyhome"
-    username = "simplecli"
-    port = 6667  # TODO add handling for cmdline args, handling of SASL port?
+class ircagent:
+    def __init__(self, server, port, init_channel, username):
+        self.server = server
+        self.init_channel = init_channel
+        self.username = username
+        self.port = port
 
     # is this a second eventloop? for prompt-toolkit?
     eventloop = create_asyncio_eventloop()
@@ -49,26 +50,24 @@ async def main(loop):
         application=create_prompt_application('Prompt: '),
         eventloop=eventloop)
 
-    sys.stdout = cli.stdout_proxy()  # is this a good idea?
+    # sys.stdout = cli.stdout_proxy()  # is this a good idea?
 
-    sockreader, sockwriter = await asyncio.open_connection(
-        server, port, loop=loop)
-    print("Socket connected.\n{}".format(repr(sockreader)))
+    async def startagent(self):
+        self.sockreader, self.sockwriter = await asyncio.open_connection(
+            self.server, self.port, loop=loop)
+        print("Socket connected.\n{}".format(repr(self.sockreader)))
+        await asyncio.ensure_future(self.socket_data_handler(loop))
+        # await asyncio.ensure_future(self.interactive_shell(loop))
 
-    async def interactive_shell():  # the gui thread?
+    async def interactive_shell(self):  # the gui thread?
         # jonothanslenders refers to `interactive_shell`? better solution?
         # credit to him @ python-prompt-toolkit for this part
-        cprint("INPUT SHELL STARTED", "red")
         asyncio.Task.current_task().name = 'SHELL'  # e.e
         while True:
-            # a bit hard to decipher, but this tests to see
-            # if socket_data_handler's task(task 0) has run it's course
-            if tasks[0].done():
-                break
             try:
-                result = await cli.run_async()  # it takes input! :D
+                result = await self.cli.run_async()  # it takes input! :D
                 if result is not None:  # if user enters '', do nothin
-                    await encode_send(result.text)
+                    await self.encode_send(result.text)
 
             except (EOFError, KeyboardInterrupt):
                 return
@@ -80,39 +79,40 @@ async def main(loop):
 
             await asyncio.sleep(0)
 
-    async def encode_send(msg):
-        sockwriter.write(bytes(msg + "\r\n", "UTF-8"))
+    async def encode_send(self, msg):
+        self.sockwriter.write(bytes(msg + "\r\n", "UTF-8"))
         print("> " + msg)
 
-    async def socket_data_handler(loop):
-        await encode_send("USER {} {} {} {}".format(*[username] * 4))
-        await encode_send("NICK {}".format(username))
-        await encode_send("JOIN {}".format(init_channel))
+    async def socket_data_handler(self, loop):
+        await self.encode_send("USER {} {} {} {}".format(*[self.username] * 4))
+        await self.encode_send("NICK {}".format(self.username))
+        await self.encode_send("JOIN {}".format(self.init_channel))
         await asyncio.sleep(0)
 
         while True:
-            data = await sockreader.readline()
+            data = await self.sockreader.readline()
             msg = str(data, "UTF-8").strip()
             print('>' + msg)
             if 'PING' in msg[:4]:
-                await encode_send("PONG{}".format(msg[4:]))
+                await self.encode_send("PONG{}".format(msg[4:]))
             if ('NickServ@services'in msg)\
                and ('This nickname is registered' in msg):
-                await encode_send("privmsg nickserv :identify " +
-                                  username + " " + password)
-            if sockreader.at_eof():
+                await self.encode_send("privmsg nickserv :identify " +
+                                       self.username + " " + password)
+                # TODO ^^ fix password/authdata to be async and in-class
+            if self.sockreader.at_eof():
                 return
 
             if '!quit' in msg:
-                await encode_send('quit')
+                await self.encode_send('quit')
                 break
             await asyncio.sleep(0)
-        sockwriter.close()
+        self.sockwriter.close()
 
-    tasks = [
-        asyncio.ensure_future(socket_data_handler(loop)),
-        asyncio.ensure_future(interactive_shell())
-    ]
+
+async def main(loop):
+    agent = ircagent('irc.freenode.net', 6667, '#nobodyhome', 'simplecli')
+    await agent.startagent()
 
 asyncio.ensure_future(main(loop))
 loop.run_forever()
