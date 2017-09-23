3d15d44795918d3b17c03e579df70e0d7110b500
DaPinkOne <crystalshade@gmail.com>
2017-09-23 00:45:08 -0400

    moved TODOs/features into its own file. added report function for debugging and graceful closing.

--- ircclient.py
+++ ircclient.py
@@ -1,22 +1,7 @@
 #!/usr/bin/python
 """
-Irc client. lets take it slow.
-Planned milestones:
-DONE: 1. connect to server, respond to ping so as not to timeout.
-DONE: 2. take  input to be sent to the server
-3. reasonable formatting for output of data from socket
-4. SSL/SASL support
-5. addition of ncurse or similar gui, to graphically separate input and output
-DONE: 5.1 converted to asyncio.
-6. support for /join, /part, /quit like most IRC clients have
-7. proper parsing of user input, rather than input->socket
-8. status line - showing which channels are currently joined, nick, etc
-9. window switching(/show #bots, switches to display #bots, hides other data,
-   temporarily
-10. vertical split, horizontal split, pane-based status bars
-    (color code/hilight active pane?)
-11. /load script.py, and /scripts/ directory for autoloading python extensions
-12. think of more stuff to add.
+Irc client implemented with python 3 + asyncio.
+
 """
 import asyncio
 from prompt_toolkit.interface import CommandLineInterface
@@ -25,8 +10,10 @@ from prompt_toolkit.shortcuts import create_prompt_application, create_asyncio_e
 import sys
 # from termcolor import colored, cprint  # for pretty colors # ;_; broken?
 
+debugmode = True
+
 loop = asyncio.get_event_loop()
-loop.set_debug(True) # does this even do something?
+loop.set_debug(debugmode)  # does this even do something?
 
 # the only non-async stuff in the whole program. #FIXME ?
 authdata = open('./authdata.txt', 'r')
@@ -41,21 +28,29 @@ class ircagent:
         self.username = username
         self.port = port
 
-    # is this a second eventloop? for prompt-toolkit?
-    eventloop = create_asyncio_eventloop()
-
-    cli = CommandLineInterface(
-        application=create_prompt_application('Prompt: '),
-        eventloop=eventloop)
-
-    sys.stdout = cli.stdout_proxy()  # is this a good idea?
 
     async def startagent(self):
+        # gui stuff to get the CLI prompt-toolkit started
+        # is this a second eventloop? for prompt-toolkit?
+        self.eventloop = create_asyncio_eventloop()
+
+        self.cli = CommandLineInterface(
+            application=create_prompt_application('Prompt: '),
+            eventloop=self.eventloop)
+
+        sys.stdout = self.cli.stdout_proxy()  # is this a good idea?
+
+        # setting up the sockstream objects
         self.sockreader, self.sockwriter = await asyncio.open_connection(
             self.server, self.port, loop=loop)
+
         print("Socket connected.\n{}".format(repr(self.sockreader)))
+
+        # adding everything to the loop proper
         asyncio.ensure_future(self.interactive_shell())
         asyncio.ensure_future(self.socket_data_handler(loop))
+
+
 
     async def interactive_shell(self):  # the gui thread?
         # jonothanslenders refers to `interactive_shell`? better solution?
@@ -64,18 +59,9 @@ class ircagent:
         while True:
             try:
                 result = await self.cli.run_async()  # it takes input! :D
-                if result is not None:  # if user enters '', do nothin
-                    await self.encode_send(result.text)
-
-            except (EOFError, KeyboardInterrupt):
-                return
-            for x in asyncio.Task.all_tasks(loop=loop):
-                try:
-                    # This is neat, and possibly a sin? use later.
-                    pass  # print(x.name)
-                except:
-                    pass
-
+                await self.encode_send(result.text)
+            except (EOFError):
+                break
             await asyncio.sleep(0)
 
     async def encode_send(self, msg):
@@ -91,7 +77,7 @@ class ircagent:
         while True:
             data = await self.sockreader.readline()
             msg = str(data, "UTF-8").strip()
-            print('>' + msg)
+            print('<' + msg)
             if 'PING' in msg[:4]:
                 await self.encode_send("PONG{}".format(msg[4:]))
             if ('NickServ@services'in msg)\
@@ -108,10 +94,28 @@ class ircagent:
             await asyncio.sleep(0)
         self.sockwriter.close()
 
+async def report(loop):
+    # this coroutine is going to monitor the tasks on the loop
+    # and report the running tasks periodically
+    # for debug purposes, and so we can close gracefully
+    # when that seems reasonable.
+    while True:
+        running_tasks = [t for t in asyncio.Task.all_tasks(loop=loop)
+                         if not t.done()]
+        if(len(running_tasks) == 1):  # we're solo? job's done.
+            print("Job's done. Quiting.")
+            # closing the loop at next "free" moment
+            loop.stop()
+            # we're done, so return to close the kill the coroutine
+            return
+        if debugmode:
+            task_names = [k._coro.__name__ for k in running_tasks]
+            print("REPORT:" + repr(task_names))
+        await asyncio.sleep(3)
 
 async def main(loop):
     agent = ircagent('irc.freenode.net', 6667, '#nobodyhome', 'simplecli')
     await agent.startagent()
-
+asyncio.ensure_future(report(loop))
 asyncio.ensure_future(main(loop))
 loop.run_forever()
